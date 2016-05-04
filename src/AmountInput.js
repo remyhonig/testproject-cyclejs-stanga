9a59c2d5827f986513921d9f0af3621eb489158f
Remy Honig <remy.honig@gmail.com>
2016-05-04 14:14:08 +0200

    Reformat code

--- src/AmountInput.js
+++ src/AmountInput.js
@@ -14,70 +14,73 @@ const SetPendingAmount = ({id, amount}) => ({
 
 
 const Amount = (top, left, amount, dirty) => box({
-    top, left,
-    height: 1,
-    width: 12,
-    align: 'right',
-    fg: dirty ? 'white' : (amount < 0) ? 'red' : 'green',
-    bg: dirty ? 'blue' : 'black',
-    tags: true,
-    content: '€ ' + curr(
-        amount / 100,
-        {
-            symbol: '€',
-            decimalSeparator: ',',
-            groupingSeparator: '.',
-            decimalPlaces: 2,
-            format: '%v'
-        }).pval
+  top, left,
+  height: 1,
+  width: 12,
+  align: 'right',
+  fg: dirty ? 'white' : (amount < 0) ? 'red' : 'green',
+  bg: dirty ? 'blue' : 'black',
+  tags: true,
+  content: '€ ' + curr(
+    amount / 100,
+    {
+      symbol: '€',
+      decimalSeparator: ',',
+      groupingSeparator: '.',
+      decimalPlaces: 2,
+      format: '%v'
+    }).pval
 });
 
-export default function({Model, props$, keys$}) {
+export default function ({Model, props$, keys$}) {
 
-    const amount$ = keys$
-      .filter(x => R.contains(x, ['escape', 'backspace','delete','-','0','1','2','3','4','5','6','7','8','9']))
-      .withLatestFrom(Model, (key, state) => [key, state.pending.toString(), state.current.toString()])
-      .map(([key, pending, current]) => {
-        if (key == 'escape') {
-          return current;
-        }
-        if (key == '-') {
-          return R.concat('-', pending);
-        }
-        if (key == 'backspace') {
-          if (pending.length == 1) {
-            return '0';
-          }
-          return pending.slice(0, -1);
-        }
-        if (key == 'delete') {
+  const amount$ = keys$
+    .filter(x => R.contains(x, ['escape', 'backspace', 'delete', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']))
+    .withLatestFrom(Model, (key, state) => [key, state.pending.toString(), state.current.toString()])
+    .map(([key, pending, current]) => {
+      if (key == 'escape') {
+        return current;
+      }
+      if (key == '-') {
+        return R.concat('-', pending);
+      }
+      if (key == 'backspace') {
+        if (pending.length == 1) {
           return '0';
         }
-        return R.concat(pending, key);
-      })
-      // http://stackoverflow.com/questions/16880327/why-am-i-getting-weird-result-using-parseint-in-node-js-different-result-from
-      .map(x => parseInt(x, 10))
-      .distinctUntilChanged()
-      .publish();
+        return pending.slice(0, -1);
+      }
+      if (key == 'delete') {
+        return '0';
+      }
+      return R.concat(pending, key);
+    })
+    // http://stackoverflow.com/questions/16880327/why-am-i-getting-weird-result-using-parseint-in-node-js-different-result-from
+    .map(x => parseInt(x, 10))
+    .distinctUntilChanged()
+    .publish();
 
-    const enteredAmount$ = amount$.sample(keys$.filter(x => x == 'enter')).distinctUntilChanged();
+  const enteredAmount$ = amount$.sample(keys$.filter(x => x == 'enter')).distinctUntilChanged();
 
-    const currentMod$ = enteredAmount$.map(x => state => ({...state, current: x}));
-    const pendingMod$ = amount$.map(x => state => ({...state, pending: x}));
+  const currentMod$ = enteredAmount$.map(x => state => ({...state, current: x}));
+  const pendingMod$ = amount$.map(x => state => ({...state, pending: x}));
 
-    const currentEvent$ = enteredAmount$.withLatestFrom(Model, (amount, model) => SetCurrentAmount({id: model.id, amount}));
-    const pendingEvent$ = amount$.withLatestFrom(Model, (amount, model) => SetPendingAmount({id: model.id, amount}));
+  const currentEvent$ = enteredAmount$.withLatestFrom(Model, (amount, model) => SetCurrentAmount({
+    id: model.id,
+    amount
+  }));
+  const pendingEvent$ = amount$.withLatestFrom(Model, (amount, model) => SetPendingAmount({id: model.id, amount}));
 
-    amount$.connect();
+  amount$.connect();
 
-    const gui$ = O.combineLatest(
-        Model, props$,
-        (state, props) => Amount(props.top, props.left, state.pending, state.pending !== state.current)
-    );
+  const gui$ = O.combineLatest(
+    Model, props$,
+    (state, props) => Amount(props.top, props.left, state.pending, state.pending !== state.current)
+  );
 
-    return {
-        Terminal: gui$,
-        Model: O.merge(Model.mod(pendingMod$), Model.mod(currentMod$)),
-        Events: O.merge(currentEvent$, pendingEvent$)
-    }
+  return {
+    Terminal: gui$,
+    Model: O.merge(Model.mod(pendingMod$), Model.mod(currentMod$)),
+    Events: O.merge(currentEvent$, pendingEvent$)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -13,97 +13,97 @@ let screen = blessed.screen({smartCSR: true, useBCE: true, title: 'Budget'});
 
 let Header = (top, left) => box(
   {
-      top, left,
-      width: '100%',
-      height: 1,
-      bg: 'black',
-      border: false
+    top, left,
+    width: '100%',
+    height: 1,
+    bg: 'black',
+    border: false
   },
   [
-      text({top: 0, left: 1, tags: true, content: '{bold}Categorie{/}'}),
-      text({top: 0, left: 22, tags: true, content: '{bold}Bedrag{/}'})
+    text({top: 0, left: 1, tags: true, content: '{bold}Categorie{/}'}),
+    text({top: 0, left: 22, tags: true, content: '{bold}Bedrag{/}'})
   ]
 );
 
 let Envelope = (top, left, title, amount, hasFocus) => box(
   {
-      top, left,
-      width: '100%',
-      height: 1,
-      bg: hasFocus ? '#004444' : 'black',
-      border: false
+    top, left,
+    width: '100%',
+    height: 1,
+    bg: hasFocus ? '#004444' : 'black',
+    border: false
   },
   [
-      Text(0, 0, title),
-      Amount(0, 22, amount)
+    Text(0, 0, title),
+    Amount(0, 22, amount)
   ]
 );
 
 let Table = (top, left, envelopes) => box(
   {
-      top, left,
-      width: 60,
-      height: 20,
-      bg: 'black',
-      padding: 0
+    top, left,
+    width: 60,
+    height: 20,
+    bg: 'black',
+    padding: 0
   },
   [
-      Header(0, 0),
-      ...envelopes
+    Header(0, 0),
+    ...envelopes
   ]
 );
 
 run(({ term, M }) => {
 
-    const keys$ = term.on('keypress').pluck(1, 'full').publish();
+  const keys$ = term.on('keypress').pluck(1, 'full').publish();
 
-    const amountInputs$ = M.lens('envelopes').liftListById((id, amountInput$) =>
-        AmountInput({Model: amountInput$, props$: O.of({left: 30, top: id}), keys$})
-    );
+  const amountInputs$ = M.lens('envelopes').liftListById((id, amountInput$) =>
+    AmountInput({Model: amountInput$, props$: O.of({left: 30, top: id}), keys$})
+  );
 
-    keys$.connect();
+  keys$.connect();
 
-    //flatMerge(amountInputs$, "Events").Events.forEach(console.log);
+  //flatMerge(amountInputs$, "Events").Events.forEach(console.log);
 
-    return {
-        term: flatCombine(amountInputs$, "Terminal").Terminal
-          .map(inputs =>
-            box(
-              {
-                top: 1,
-                left: 1,
-                width: '100%',
-                height: '100%',
-                bg: 'grey',
-                border: true
-              },
-              inputs
-            )
-          ),
-        exit: term.on('key C-c'),
-        M: flatMerge(amountInputs$, "Model").Model
-    }
-}, {
-    term: makeTermDriver(screen),
-    exit: exit$ => exit$.forEach(::process.exit),
-    M: Model(
-      {
-        envelopes: [
+  return {
+    term: flatCombine(amountInputs$, "Terminal").Terminal
+      .map(inputs =>
+        box(
           {
-            'id': 1,
-            'current': 123,
-            'pending': 123
+            top: 1,
+            left: 1,
+            width: '100%',
+            height: '100%',
+            bg: 'grey',
+            border: true
           },
-          {
-            'id': 2,
-            'current': 456,
-            'pending': 456
-          }
-        ]
-      },
-      {
-        logging: false,
-        info: (...args) => console.info(...args)
-      }
-    )
+          inputs
+        )
+      ),
+    exit: term.on('key C-c'),
+    M: flatMerge(amountInputs$, "Model").Model
+  }
+}, {
+  term: makeTermDriver(screen),
+  exit: exit$ => exit$.forEach(::process.exit),
+  M: Model(
+    {
+      envelopes: [
+        {
+          'id': 1,
+          'current': 123,
+          'pending': 123
+        },
+        {
+          'id': 2,
+          'current': 456,
+          'pending': 456
+        }
+      ]
+    },
+    {
+      logging: false,
+      info: (...args) => console.info(...args)
+    }
+  )
 });
